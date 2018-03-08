667f6fe590b540b93c55bd4309c6056999ba1f99
vijay <vijay@logiconglobal.com>
2018-03-08 15:50:54 +0530

    merge code from kiran changes to git

--- RMA.Web/Controllers/COOController.cs
+++ RMA.Web/Controllers/COOController.cs
@@ -27,7 +27,7 @@ namespace RMA.Web.Controllers
 
         public ActionResult COOHeaderList()
         {
-            var cooHeaderList = new COOHeaderBO().GetList(BRANCH_ID);
+            var cooHeaderList = new COOHeaderBO().GetList(BRANCH_ID).OrderByDescending(x => x.DocumentNo).ToList();
             return View(cooHeaderList);
         }
 
@@ -40,10 +40,11 @@ namespace RMA.Web.Controllers
 
             var branchprofile = new BranchBO().GetBranch(new Branch { BranchCode = branchCode.BranchCode });
 
-
-            cooHeader.ExporterName = branchprofile.CompanyCode;
-            //cooHeader.ExporterAddress = branchprofile.BranchAddress.Address1 + "\n" + branchprofile.BranchAddress.Address2 + "\n" + branchprofile.BranchAddress.CityName + "\n" + branchprofile.BranchAddress.StateName + "\n" + branchprofile.BranchAddress.ZipCode + "\n" + branchprofile.BranchName;
-            cooHeader.ExporterAddress = branchprofile.BranchAddress.FullAddress.Replace(",", "\n");
+            // var companyFullName = new CompanyBO().GetCompany(new Company { CompanyCode = branchprofile.CompanyCode }).CompanyName;
+            //var countryName = new CountryBO().GetCountry(new Country { CountryCode = branchprofile.BranchAddress.CountryCode }).CountryName;
+            cooHeader.ExporterName = "EZY INFOTECH PTE LTD";// companyFullName;
+            cooHeader.ExporterAddress = "1 CHANGI NORTH STREET 1 \n SINGAPORE 498789";//branchprofile.BranchAddress.Address1 + "\n" + countryName + "   " + branchprofile.BranchAddress.ZipCode;
+            //cooHeader.ExporterAddress = branchprofile.BranchAddress.FullAddress.Replace(",", "\n");
             ViewBag.FileName = "";
             if (DocumentNo == "")
             {
@@ -52,6 +53,7 @@ namespace RMA.Web.Controllers
                 cooHeader.DepartureDate = DateTime.Now;
                 cooHeader.CreatedOn = DateTime.Now;
                 cooHeader.IsInvoiceConfirm = true;
+                cooHeader.IsCertified = false;
 
             }
             else
@@ -124,7 +126,7 @@ namespace RMA.Web.Controllers
                     {
                         ViewData["DuplicateSNo"] = DuplicateSNos;
                     }
-                    ViewBag.FileName = fileName;
+
                 }
             }
 
@@ -167,6 +169,7 @@ namespace RMA.Web.Controllers
             var DA = new OleDbDataAdapter();
             var DS = new DataSet();
             var ext = Path.GetExtension(fileName);
+            var _fileName = Path.GetFileName(fileName);
             var strxlsProvider = string.Empty;
             short rowCount;
 
@@ -214,17 +217,34 @@ namespace RMA.Web.Controllers
                     rowCount = Convert.ToInt16(DS.Tables[0].Rows.Count);
 
                     var tableHeaderValue = DS.Tables[0].Columns[0].ColumnName;
-                    //lstInvoice.Add(new COODetail { ModelNo = tableHeaderValue.Trim() });
-                    foreach (DataRow row in DS.Tables[0].Rows)
+                    if (Convert.ToInt32(DS.Tables[0].Columns.Count) == 4)
                     {
-                        var currentItem = new COODetail();
+                        if (DS.Tables[0].Columns[0].ColumnName.ToString().ToLower() == "model")//&& DS.Tables[0].Columns[1].ColumnName.ToString().ToLower() == "description" && DS.Tables[0].Columns[2].ColumnName.ToString().ToLower() == "quantity (in pcs)" && DS.Tables[0].Columns[3].ColumnName.ToString().ToLower() == "origin")
+                        {
+                            foreach (DataRow row in DS.Tables[0].Rows)
+                            {
+                                var currentItem = new COODetail();
 
-                        currentItem.ModelNo = row[0].ToString().Trim();
-                        currentItem.Description = row[1].ToString().Trim();
-                        currentItem.Qty = Convert.ToInt16(row[2].ToString().Trim());
-                        currentItem.Origin = row[3].ToString().Trim();
+                                if (row[0].ToString() != "")
+                                {
+                                    currentItem.ModelNo = row[0].ToString().Trim();
+                                    currentItem.Description = row[1].ToString().Trim();
+                                    currentItem.Qty = Convert.ToInt16(row[2].ToString().Trim());
+                                    currentItem.Origin = row[3].ToString().Trim();
 
-                        lstInvoice.Add(currentItem);
+                                    lstInvoice.Add(currentItem);
+                                }
+                            }
+                            ViewBag.FileName = _fileName;
+                        }
+                        else
+                        {
+                            ViewBag.WrongFileContent = true;
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.WrongFileContent = true;
                     }
                 }
 
@@ -243,13 +263,12 @@ namespace RMA.Web.Controllers
         }
 
 
-
-
         [HttpPost]
         public ActionResult SaveCOODetails(COODetail cooDetails)
         {
             cooDetails.BranchID = BRANCH_ID;
             var result = new COODetailBO().SaveCOODetail(cooDetails);
+
             return RedirectToAction("COODetailsList", "COO");
         }
 
@@ -257,15 +276,27 @@ namespace RMA.Web.Controllers
         public ActionResult SaveCOOHeader(COOHeader cooHeader)
         {
             cooHeader.BranchID = BRANCH_ID;
+
+            //if (cooHeader.IsInvoiceConfirm == true)
+            //    cooHeader.IsCertified = false;
+            //else if (cooHeader.IsCertified == true)
+            //    cooHeader.IsInvoiceConfirm = false;
+
             var result = new COOHeaderBO().SaveCOOHeader(cooHeader);
-            return RedirectToAction("COOHeaderList", "COO");
+            var latestDocumentNo = "";
+            if (result)
+            {
+                TempData["COOSaved"] = result;
+                var list = new COOHeaderBO().GetList(BRANCH_ID).OrderByDescending(x => x.DocumentNo).Select(x => x.DocumentNo).ToList().Take(1);
+                latestDocumentNo = cooHeader.DocumentNo != null ? cooHeader.DocumentNo : list.ToList()[0];
+            }
+            return RedirectToAction("EditCOOHeader", "COO", new { DocumentNumber = latestDocumentNo });
         }
 
         [HttpPost]
         public JsonResult GetCOOHeaderbySearch(COOSearch coo)
         {
-            var result = new COOHeaderBO().SearchList(BRANCH_ID, coo.VesselName, coo.ConsigneeName, coo.FromDate, coo.ToDate);
-
+            var result = new COOHeaderBO().SearchList(BRANCH_ID, coo.VesselName, coo.ConsigneeName, coo.FromDate, coo.ToDate).OrderByDescending(x => x.DocumentNo).ToList();
             return Json(result, JsonRequestBehavior.AllowGet);
         }
 
@@ -283,7 +314,10 @@ namespace RMA.Web.Controllers
             var cooHeader = new COOHeader();
             cooHeader.BranchID = BRANCH_ID;
             cooHeader.DocumentNo = DocumentNumber;
-            var cooHeaderDetails = new COOHeaderBO().DeleteCOOHeader(cooHeader);
+            var result = new COOHeaderBO().DeleteCOOHeader(cooHeader);
+
+            TempData["COODeleted"] = result;
+
             return RedirectToAction("COOHeaderList", "COO");
         }
 
@@ -302,7 +336,7 @@ namespace RMA.Web.Controllers
         [HttpPost]
         public JsonResult GetCOOs(DataTableObject Obj)
         {
-            var cooList = new COOHeaderBO().GetList(BRANCH_ID);
+            var cooList = new COOHeaderBO().GetList(BRANCH_ID).OrderByDescending(x => x.DocumentNo).ToList();
             var totalRecords = new COOHeaderBO().GetList(BRANCH_ID).Count();
 
             return Json(new { coos = cooList, totalRecords = totalRecords }, JsonRequestBehavior.AllowGet);
@@ -311,6 +345,7 @@ namespace RMA.Web.Controllers
 
         public FileResult PrintCOOHeaderReport(string branchID, string documentNo)
         {
+            PageNo = 1;
             try
             {
                 var outputPdfStream = new MemoryStream();
@@ -340,7 +375,6 @@ namespace RMA.Web.Controllers
                 throw;
             }
 
-
         }
 
 
@@ -358,7 +392,7 @@ namespace RMA.Web.Controllers
             for (int i = 0; i < cooHeaderItem.COODetails.Count();)
             {
 
-                path = System.Web.Hosting.HostingEnvironment.MapPath("~/COOHeaderTemplate/DOC250118.pdf");
+                path = System.Web.Hosting.HostingEnvironment.MapPath("~/COOHeaderTemplate/COOPdf.pdf");
 
                 PdfReader reader = new PdfReader(path);
 
@@ -386,6 +420,12 @@ namespace RMA.Web.Controllers
         {
             try
             {
+                var IsConfirmation = "";
+                if (cooHeader.IsInvoiceConfirm == true)
+                    IsConfirmation = "THIS C/O IS APPLIED TO INVOICE NO.  " + cooHeader.InvoiceNo + "           Date:  " + cooHeader.InvoiceDate.ToShortDateString() + "";
+                else
+                    IsConfirmation = "WE HEREBY CERTIFY THAT THE ABOVE MENTIONED GOODS ARE ORIGIN OF ";
+
                 pdfFormFields.SetField("ExporterName", cooHeader.ExporterName + "\n" + cooHeader.ExporterAddress);  //\nExch: " + declarationHeader.CurrencyExRate.ToString("#0.0000")
                 pdfFormFields.SetField("ConsigneeName", cooHeader.ConsigneeName + (cooHeader.ConsigneeAddress != null ? "\n" + cooHeader.ConsigneeAddress : ""));
 
@@ -397,13 +437,20 @@ namespace RMA.Web.Controllers
                 pdfFormFields.SetField("Name", cooHeader.DeclarantName);
                 pdfFormFields.SetField("Designation", cooHeader.Designation);
                 pdfFormFields.SetField("Date", cooHeader.CreatedOn.ToShortDateString());
-                pdfFormFields.SetField("InvoiceNo", cooHeader.InvoiceNo != null ? cooHeader.InvoiceNo.ToString() : "");
-                pdfFormFields.SetField("InvoiceDate", cooHeader.InvoiceDate != null ? cooHeader.InvoiceDate.ToShortDateString() : "");
+                //pdfFormFields.SetField("InvoiceNo", cooHeader.InvoiceNo != null ? cooHeader.InvoiceNo.ToString() : "");
+                //pdfFormFields.SetField("InvoiceDate", (cooHeader.IsCertified != true && cooHeader.InvoiceDate != null) ? cooHeader.InvoiceDate.ToShortDateString() : "");
                 pdfFormFields.SetField("MODEL", "MODEL");
                 pdfFormFields.SetField("DESCRIPTION", "DESCRIPTION");
                 pdfFormFields.SetField("QUANTITY", "QUANTITY(Pieces)");
                 pdfFormFields.SetField("ORIGIN", "ORIGIN");
                 pdfFormFields.SetField("PageNo", PageNo.ToString());
+                pdfFormFields.SetField("IsConformation", IsConfirmation.ToString());
+                pdfFormFields.SetField("Country1", cooHeader.IsInvoiceConfirm != true ? cooHeader.Country1 != null ? cooHeader.Country1.ToString() : "" : "");
+                pdfFormFields.SetField("Country2", cooHeader.IsInvoiceConfirm != true ? cooHeader.Country2 != null ? cooHeader.Country2.ToString() : "" : "");
+                pdfFormFields.SetField("Country3", cooHeader.IsInvoiceConfirm != true ? cooHeader.Country3 != null ? cooHeader.Country3.ToString() : "" : "");
+                pdfFormFields.SetField("Country4", cooHeader.IsInvoiceConfirm != true ? cooHeader.Country4 != null ? cooHeader.Country4.ToString() : "" : "");
+                pdfFormFields.SetField("Country5", cooHeader.IsInvoiceConfirm != true ? cooHeader.Country5 != null ? cooHeader.Country5.ToString() : "" : "");
+                pdfFormFields.SetField("Country6", cooHeader.IsInvoiceConfirm != true ? cooHeader.Country6 != null ? cooHeader.Country6.ToString() : "" : "");
                 int count = dcount;
                 for (int i = 0; i < validcount; i++)
                 {
@@ -432,7 +479,7 @@ namespace RMA.Web.Controllers
             var customerDetails = new CustomerBO().GetCustomer(new Customer { BranchID = BRANCH_ID, CustomerCode = customerList.CustomerCode });
             var countryName = new CountryBO().GetCountry(new Country { CountryCode = customerDetails.CountryCode }).CountryName;
             //var fullAddress = customerDetails.Address1 + "\n" + customerDetails.Address2 + "\n" + customerDetails.State + "\n" + countryName + "\n" + customerDetails.PostCode;
-            var consigneeAddress = new ConsigneeAddress() { CustomerName = customerDetails.CustomerName, Address1 = customerDetails.Address1, Address2 = customerDetails.Address2, State = customerDetails.State, Country = countryName, PostCode = customerDetails.PostCode };
+            var consigneeAddress = new ConsigneeAddress() { CustomerName = customerDetails.CustomerName, Address1 = customerDetails.Address1, Address2 = customerDetails.Address2, State = customerDetails.State, Country = countryName, PostCode = (customerDetails.PostCode != "NA" && customerDetails.PostCode != "NP") ? customerDetails.PostCode : "" };
             return Json(consigneeAddress, JsonRequestBehavior.AllowGet);
         }
 
@@ -462,7 +509,6 @@ namespace RMA.Web.Controllers
 
     public class ConsigneeAddress
     {
-
         public string CustomerName { get; set; }
         public string Address1 { get; set; }
         public string Address2 { get; set; }
