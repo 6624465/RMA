fed7dce2000da69fd103cabb8bf70e485e0d4769
vijay <vijay@logiconglobal.com>
2018-03-12 14:08:20 +0530

    root level securables implemented

--- RMA.Web/Controllers/HomeController.cs
+++ RMA.Web/Controllers/HomeController.cs
@@ -7,13 +7,43 @@ using EZY.RMAS.BusinessFactory;
 using EZY.RMAS.Contract;
 namespace RMA.Web.Controllers
 {
+    public class HomeVm
+    {
+        public bool IsMaster { get; set; } = false;
+        public bool IsTransactions { get; set; } = false;
+        public bool IsAdministration { get; set; } = false;
+        public bool IsReports { get; set; } = false;
+    }
+
     [WebSsnFilter]
     public class HomeController : BaseController
     {
         [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var userSecurables = USER_SECURABLES;
+            var homeVm = new HomeVm();
+            if (userSecurables != null && userSecurables.Count > 0)
+            {
+                homeVm.IsMaster = userSecurables
+                                    .Where(x => x.GroupID == "Master")
+                                    .Count() > 0;
+
+                homeVm.IsTransactions = userSecurables
+                                        .Where(x => x.GroupID == "Transaction")
+                                        .Count() > 0;
+
+                homeVm.IsAdministration = userSecurables
+                                        .Where(x => x.GroupID == "Administration")
+                                        .Count() > 0;
+
+                homeVm.IsReports = userSecurables
+                                        .Where(x => x.GroupID == "Reports")
+                                        .Count() > 0;
+            }
+
+
+            return View(homeVm);
         }
         public ActionResult Administration()
         {
